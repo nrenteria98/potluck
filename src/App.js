be78e933c4ff7768316d2629ea8b19fcb57de08f
nrenteria <nicolas.renteria16@gmail.com>
2022-01-08 14:10:19 -0500

    Changed BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Home from './pages/Home';
 import Search from './pages/Search';
 import Bubbles from './components/Bubbles';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import './style/App.scss';
 require('dotenv').config()
 
