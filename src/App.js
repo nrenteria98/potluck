2dc5056bc2694fcd2950de099822e1d26b7e8393
nrenteria <nicolas.renteria16@gmail.com>
2022-01-08 15:35:21 -0500

    attempt to get browserRouter to work in gitpages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import Home from './pages/Home';
 import Search from './pages/Search';
 import Bubbles from './components/Bubbles';
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './style/App.scss';
 require('dotenv').config()
 
 function App() {
   return (
     <div className="Main">
-      <Router>
+      <Router basename='/potluck'>
         <Routes>
-          <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </Router>
       {/* Bubbles for background animation */}
