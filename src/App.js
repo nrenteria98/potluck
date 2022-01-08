07ad9f10d04d91b220e82587bf6689f096c1b381
nrenteria <nicolas.renteria16@gmail.com>
2022-01-08 14:36:04 -0500

    Attempt again to get routing to work

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import Home from './pages/Home';
 import Search from './pages/Search';
 import Bubbles from './components/Bubbles';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import './style/App.scss';
 require('dotenv').config()
 
 function App() {
   return (
     <div className="Main">
-      <Router basename='/potluck'>
+      <Router>
         <Routes>
-          <Route path="/search" element={<Search />} />
           <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
         </Routes>
       </Router>
       {/* Bubbles for background animation */}
